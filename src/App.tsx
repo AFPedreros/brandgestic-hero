cbed8a8d7ded17853e12e33eb7b3a9c2d93d0371
AFPedreros <felipe.pedreros94@gmail.com>
2024-03-11 17:23:43 -0500

    "first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "@/components/ui/button";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div className="relative bg-red-200 flex h-screen w-full">
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
